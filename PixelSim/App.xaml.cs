def56ed0cb28e8f344d53e4701c613c5aa1f0109
quartermeat <jeremy.williamson83@gmail.com>
2017-06-13 23:54:53 -0400

    A commit before bed.

--- PixelSim/App.xaml.cs
+++ PixelSim/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using PixelSim.Controller;
 
 namespace PixelSim
 {
@@ -13,5 +14,6 @@ namespace PixelSim
     /// </summary>
     public partial class App : Application
     {
+        MainController mainController = new MainController();
     }
 }
--- PixelSim/MainWindow.xaml.cs
+++ PixelSim/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using CellularAutomata.Model;
 
 namespace PixelSim
 {
@@ -7,10 +9,21 @@ namespace PixelSim
     /// </summary>
     public partial class MainWindow : Window
     {
+        public event EventHandler MapButtonPressed;
+        public event EventHandler StartButtonPressed;
+        public event EventHandler ShowStatusBoxMenuItemClicked;
+        public event EventHandler ChangedTimerTrackBar;
+        public event EventHandler GraphicsButtonPressed;
+
         public MainWindow()
         {
             InitializeComponent();
         }
-        
+
+        public void SetupMap()
+        {
+            mapPanel.Width = Map.Width * Map.Resolution;
+            mapPanel.Height = Map.Height * Map.Resolution;
+        }
     }
 }
